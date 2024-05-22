88a853186040bd29d7ca8322bda655df7cfa9405
micheledibi <micheledibi2002@gmail.com>
2024-05-22 19:34:31 +0200

    Modificato main

--- fattureincloud/src/main/java/com/fattureincloud/Main.java
+++ fattureincloud/src/main/java/com/fattureincloud/Main.java
@@ -1,71 +1,187 @@
 package com.fattureincloud;
 
-import java.security.SecureRandom;
+import java.math.BigDecimal;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
+import java.util.List;
 
+import io.vavr.control.Either;
+
+import java.io.IOException;
+
+import it.fattureincloud.sdk.ApiClient;
+import it.fattureincloud.sdk.Configuration;
+import it.fattureincloud.sdk.api.IssuedDocumentsApi;
+import it.fattureincloud.sdk.auth.OAuth;
 import it.fattureincloud.sdk.auth.OAuth2AuthorizationCodeError;
 import it.fattureincloud.sdk.auth.OAuth2AuthorizationCodeManager;
 import it.fattureincloud.sdk.auth.OAuth2AuthorizationCodeParams;
 import it.fattureincloud.sdk.auth.OAuth2AuthorizationCodeResponse;
 import it.fattureincloud.sdk.auth.Scope;
+import it.fattureincloud.sdk.model.CreateIssuedDocumentRequest;
+import it.fattureincloud.sdk.model.CreateIssuedDocumentResponse;
+import it.fattureincloud.sdk.model.Entity;
+import it.fattureincloud.sdk.model.IssuedDocument;
+import it.fattureincloud.sdk.model.IssuedDocumentItemsListItem;
+import it.fattureincloud.sdk.model.IssuedDocumentType;
 
-import java.util.List;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.net.URI;
 
-import io.vavr.control.Either;
+import java.awt.Desktop;
 
 public class Main {
-    public static void main(String[] args) {
-        String redirectUri = "http://localhost:3000/oauth";
-        OAuth2AuthorizationCodeManager oauth = new OAuth2AuthorizationCodeManager(
-            "", 
-            "", 
-            redirectUri);
 
+    // private static int companyId = 850680; // ENTE DI RICERCA SCIENTIFICA ED ALTA FORMAZIONE
+    private static int companyId = 1169419; // UNIVERSO SRL
+
+    private static String clientId = "CYLs5KUab44D1abl3vZjdhcI9PjWRNIR";
+    private static String clientSecret = "j5dGmQs7LBqTxTnC0qYBiZc805Cnc32l36gaQr7yjc1eBzEsQQQYQPMmhhnG9P15";
+
+    public static void main(String[] args) {
+
+        // Inizializzazione dell'oggetto OAuth2AuthorizationCodeManager
+        String redirectURI = "http://localhost:8000";
+        OAuth2AuthorizationCodeManager oauth = new OAuth2AuthorizationCodeManager(clientId, clientSecret, redirectURI);
+
+        // Reindirizzamento dell'utente alla pagina di autorizzazione di fatture in
+        // cloud
         List<Scope> scopes = Arrays.asList(Scope.SETTINGS_ALL, Scope.ISSUED_DOCUMENTS_INVOICES_READ);
 
+        try {
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(oauth.getAuthorizationUrl(scopes, "EXAMPLE_STATE")));
+            }
+
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/", new HttpHandler() {
+                @Override
+                public void handle(HttpExchange exchange) {
+                    URI requestURI = exchange.getRequestURI();
+                    String query = requestURI.getQuery();
+
+                    // String urlAuthorization = new BufferedReader(new
+                    // InputStreamReader(System.in)).readLine();
+
+                    try {
+                        OAuth2AuthorizationCodeParams params = oauth.getParamsFromUrl("http://localhost:8000?" + query);
+
+                        String code = params.getAuthorizationCode();
+                        // String state = params.getState();
+
+                        Either<OAuth2AuthorizationCodeError, OAuth2AuthorizationCodeResponse> fetchToken = oauth
+                                .fetchToken(code);
+
+                        if (fetchToken.isRight()) {
+                            OAuth2AuthorizationCodeResponse tokenObj = fetchToken.get();
+
+                            String accessToken = tokenObj.getAccessToken();
+                            String refreshToken = tokenObj.getRefreshToken();
+
+                            System.out.println("Access Token: " + accessToken);
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println("[Main - main - Exception]: " + e.getMessage());
+                    }
+                }
+            });
+
+            server.start();
+
+        } catch (IOException e) {
+            System.out.println("[Main - main - IOException]: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[Main - main - Exception]: " + e.getMessage());
+        }
+
+        // Main m = new Main();
 
         /*
-         * Il parametro “state” è un valore che viene inviato al server di autorizzazione durante la richiesta di autorizzazione 
-         * e poi restituito indietro al client nella risposta di autorizzazione.
+         * m.creazioneFattura(
+         * companyId,
+         * "Michele",
+         * "Dibisceglia",
+         * "DBSMHL02A14C514V",
+         * "Via Giovanni Laterza 3",
+         * "Bari",
+         * "70125",
+         * "BA",
+         * "micheledibi2002@gmail.com",
+         * "Articolo di prova",
+         * "Descrizione articolo di prova",
+         * new BigDecimal(0));
          * 
-         * Nel tuo codice, “EXAMPLE_STATE” è solo un valore di esempio per il parametro “state”. 
-         * Dovresti sostituirlo con un valore generato in modo sicuro e univoco per la tua sessione o richiesta. 
-         * Se non hai bisogno di mantenere lo stato dell’applicazione, puoi semplicemente ignorare il valore restituito. 
-         * Tuttavia, dovresti comunque verificare che il valore “state” restituito corrisponda a quello inviato per mitigare gli attacchi CSRF.
          */
+    }
 
-        String stateParam = "EXAMPLE_STATE";
- 
-        /*
-        SecureRandom sr = new SecureRandom();
-        byte[] state = new byte[24];
-        sr.nextBytes(state);
+    public static void creazioneFattura(
+            String accessToken,
+            int companyId,
+            String clienteNome,
+            String clienteCognome,
+            String clienteCodiceFiscale,
+            String clienteIndirizzo,
+            String clienteCitta,
+            String clienteCAP,
+            String clienteProvincia,
+            String clienteEmail,
+            String articoloNome,
+            String articoloDescrizione,
+            BigDecimal articoloPrezzo) {
+        // Creazione della fattura
+        IssuedDocument fattura = new IssuedDocument();
 
-        stateParam = Base64.getUrlEncoder().withoutPadding().encodeToString(state);
-        */
+        fattura.setType(IssuedDocumentType.INVOICE);
+        fattura.seteInvoice(true);
 
-        System.out.println(oauth.getAuthorizationUrl(scopes, stateParam));
+        // Creazione del cliente
+        Entity cliente = new Entity();
+        cliente.setName(clienteNome + " " + clienteCognome);
+        cliente.setFirstName(clienteNome);
+        cliente.setLastName(clienteCognome);
+        cliente.setAddressCity(clienteCitta);
+        cliente.setAddressPostalCode(clienteCAP);
+        cliente.setAddressProvince(clienteProvincia);
+        cliente.setAddressStreet(clienteIndirizzo);
+        cliente.setEmail(clienteEmail);
+
+        fattura.setEntity(cliente);
+
+        // Creazione della lista di articoli
+        IssuedDocumentItemsListItem articolo = new IssuedDocumentItemsListItem();
+        articolo.setName(articoloNome);
+        articolo.setDescription(articoloDescrizione);
+
+        articolo.setNetPrice(articoloPrezzo);
+
+        List<IssuedDocumentItemsListItem> listaArticoli = new ArrayList<>();
+        listaArticoli.add(articolo);
+
+        fattura.setItemsList(listaArticoli);
 
         try {
-            OAuth2AuthorizationCodeParams params = oauth.getParamsFromUrl("http://localhost:3000/oauth?code=" + "EXAMPLE_CODE" + "&state="  + "EXAMPLE_STATE");
-        
-            String authorizationCode = params.getAuthorizationCode();
-            // String statusCode = params.getState();
+            ApiClient defaultClient = Configuration.getDefaultApiClient();
+            defaultClient.setBasePath("https://api-v2.fattureincloud.it");
 
-            Either<OAuth2AuthorizationCodeError, OAuth2AuthorizationCodeResponse> result = oauth.fetchToken(authorizationCode);
+            OAuth OAuth2AuthenticationCodeFlow = (OAuth) defaultClient
+                    .getAuthentication("OAuth2AuthenticationCodeFlow");
 
-            if (result.isRight()) {
-                OAuth2AuthorizationCodeResponse tokenObj = result.get();
-                String accessToken = tokenObj.getAccessToken();
+            OAuth2AuthenticationCodeFlow.setAccessToken(
+                    accessToken);
 
-                System.out.println(accessToken);
-            } else {
-                OAuth2AuthorizationCodeError error = result.getLeft();
-                System.out.println("error: " + error.toString());
-            }
-        }
-        catch (Exception e) {
+            IssuedDocumentsApi apiInstance = new IssuedDocumentsApi(defaultClient);
+
+            CreateIssuedDocumentRequest request = new CreateIssuedDocumentRequest();
+            request.data(fattura);
+
+            CreateIssuedDocumentResponse result = apiInstance.createIssuedDocument(companyId, request);
+
+            System.out.println(result);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
